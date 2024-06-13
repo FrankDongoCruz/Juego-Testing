3ad63f75b205a5cfe23aac843f0a8ae2305e1103
FrankDongoCruz <102604630+FrankDongoCruz@users.noreply.github.com>
2024-06-13 01:42:07 -0500

    Add files via upload

--- levels.py
+++ levels.py
@@ -2,7 +2,6 @@ import pygame
 import os
 from opciones import get_screen_dimensions
 
-# Función para cargar las texturas de los bloques
 def load_block_textures():
     block_textures = []
     bloques_dir = os.path.join(os.path.dirname(__file__), 'bloques')
@@ -10,19 +9,18 @@ def load_block_textures():
         filename = f'bloque{i}.png'
         filepath = os.path.join(bloques_dir, filename)
         block_texture = pygame.image.load(filepath).convert_alpha()
-        # Ajustar tamaño de la textura según sea necesario
-        block_texture_resized = pygame.transform.scale(block_texture, (65, 25))  # Ajustar tamaño según necesites
+        block_texture_resized = pygame.transform.scale(block_texture, (65, 25))
         block_textures.append(block_texture_resized)
     return block_textures
 
 def load_level(level):
     bricks = []
-    block_textures = load_block_textures()  # Cargar texturas de bloques
-    screen_width, screen_height = get_screen_dimensions()  # Obtener dimensiones de la pantalla
+    block_textures = load_block_textures()
+    screen_width, screen_height = get_screen_dimensions()
 
     if level == 1:
-        num_rows = 3
-        num_cols = 5
+        num_rows = 5
+        num_cols = 9
         brick_width = 65
         brick_height = 25
         level_width = num_cols * brick_width
@@ -38,8 +36,8 @@ def load_level(level):
                 bricks.append((brick_rect, block_textures[(i * num_cols + j) % len(block_textures)]))
                 
     elif level == 2:
-        num_rows = 4
-        num_cols = 5
+        num_rows = 7
+        num_cols = 11
         brick_width = 65
         brick_height = 25
         level_width = num_cols * brick_width
@@ -49,9 +47,11 @@ def load_level(level):
         start_y = 40
         
         for i in range(num_rows):
-            for j in range(num_cols):
-                brick_rect = pygame.Rect(start_x + j * brick_width, 
-                                         start_y + i * brick_height, brick_width, brick_height)
+            for j in range(num_cols - i):
+                brick_rect = pygame.Rect(start_x + j * brick_width + i * (brick_width // 2), 
+                                         start_y + i * brick_height, 
+                                         brick_width, 
+                                         brick_height)
                 bricks.append((brick_rect, block_textures[(i * num_cols + j) % len(block_textures)]))
 
     return bricks
