6cf0d7350331586894dca1a8094bcca45a703a3a
FrankDongoCruz <102604630+FrankDongoCruz@users.noreply.github.com>
2024-06-20 22:19:23 -0500

    Delete tests directory

--- tests/test_game.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import pygame
-from game import game_loop
-
-def test_game_loop(monkeypatch):
-    monkeypatch.setattr('pygame.event.get', lambda: [pygame.event.Event(pygame.KEYDOWN, {'key': pygame.K_w})])
-    assert game_loop('easy', 1) == 'next_level'
--- tests/test_opciones.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from opciones import *
-
-def test_adjust_volume():
-    initial_volume = 0.5
-    adjust_volume(0.8)
-    assert volume == 0.8
-    adjust_volume(initial_volume)
